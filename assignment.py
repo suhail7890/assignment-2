d15c0afcdcd2a1e7752e7fe299f3d2afc8743b06
Mahaboob Dilse <suhailshaik6055@gmail.com>
2023-05-15 20:37:32 +0530

    adding if else

--- assignment.py
+++ assignment.py
@@ -2,3 +2,11 @@ a='rotator'
 b='suhail'
 print(a==a[::-1])
 print(b==b[::-1])
+if a==a[::-1]:
+    print(a+ ' is a palindrome')
+else:
+    print(a+ 'is not a palindrome')
+if b==b[::-1]:
+    print(b+ 'is a palindrome')
+else:
+    print(b+ 'is not a palindrome')
